e5bdeb509b208d7d529204c05e95098cd81b158d
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-05-04 18:41:56 +0200

    IT1 done

---
+++ src/components/IdCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function IdCard(props){
+    console.log(props)
+    return(
+        <div>
+            <img src={props.picture} alt={props.firstName}/>
+            <div>
+                <b>FirstName : {props.firstName}</b><br></br>
+                <b>LastName : {props.lastName}</b><br></br>
+                <b>Gender : {props.gender}</b><br></br>
+                <b>Height : {props.height}</b><br></br>
+                <b>Birth : {props.birth.toDateString()}</b><br></br>
+            </div>
+        </div>
+    )
+}
+
+export default IdCard
