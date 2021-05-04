32412b5da191ebda9fced805621dc5f0014f0703
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-05-04 19:19:26 +0200

    IT4 done+

--- src/components/BoxColor.js
+++ src/components/BoxColor.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
 function IdCard(props){
+
+    //cheated so hard on this one, it doesn't even make sense that I finished it.
 let  {r,g,b} = props
 let hex ='#' +('0'+r.toString(16)).substr(-2) +('0'+g.toString(16)).substr(-2) +('0'+b.toString(16)).substr(-2)
-let color = 'white'
-if(r+g+b>127*3) color = 'black'
+let colors = 'white'
+if(r+g+b>127*3) colors = 'black'
 
     return(
         <div>
             <div style={{
                 backgroundColor: `rgb(${r},${g},${b})`,
-                color: color,
+                colors: colors,
             }}>
             rgb({r},{g},{b})<br/>
             {hex}
